c5f289128cd77c3f99d80a59290bd266edc85864
fakanbiomowunmi@gmail.com <fakanbiomowunmi@gmail.com>
2022-07-24 20:22:11 -0400

    Almost Done

---
+++ models/transactions.js
@@ -0,0 +1,37 @@
+module.exports = [
+    {
+        name: "Olayinka",
+        date: "05:07:2022",
+        amount: 1000,
+        from: "income"
+    },
+    {
+        name: "Doux-Doux",
+        date: "09:07:2022",
+        amount: -300,
+        from: "Taxes"
+    },
+    {
+        name: "Wajang",
+        date: "10:07:2022",
+        amount: 200,
+        from: "Retirement"
+    },
+    {
+        name: "Laayan",
+        date: "11:07:2022",
+        amount: -100,
+        from: "Savings"
+    },  {
+        name: "Ponme-Cyttere",
+        date: "08:07:2022",
+        amount: 40,
+        from: "Credit Crad Payment"
+    },  {
+        name: "Move-longe",
+        date: "01:07:2022",
+        amount: -76,
+        from: "Pet Food"
+    }
+
+]
